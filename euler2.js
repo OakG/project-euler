1866bb1dfa838c9213deb861a4d2379f45272630
OakG <oak.garcia@gmail.com>
2016-09-09 13:46:14 -0300

    Fibonacci Evens

---
+++ euler2.js
@@ -0,0 +1,16 @@
+// Even Fibonacci numbers
+var first = 1;var second = 2; //first numbers in sequence
+var placeholder = 0; //next number in sequence
+var result = 0;
+
+var limit = 4000000; //highest number in sequence
+
+while (placeholder < limit) {
+    placeholder = first + second;
+    first = second;
+    second = placeholder;
+    if (first % 2 == 0) {
+        result = result + first;
+    }
+}
+console.log(result);
