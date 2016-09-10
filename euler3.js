ae1d0afd33a4c8777b3d97cde25f5f3087c36009
OakG <oak.garcia@gmail.com>
2016-09-10 08:50:46 -0300

    Euler 3

--- euler3.js
+++ euler3.js
@@ -6,3 +6,24 @@
 //        console.log(i + " is a divisor");
 //    }
 //}
+
+var isPrime = function(number) {
+    count = 0;
+
+    for (i = 2; i <= number; i++) {
+        if (number%i === 0);
+        count =+ 1;
+    }
+    if (count === 1) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+for (n = 2; n <= target; n++) {
+    if (target%n === 0 && isPrime(n) === true) {
+        console.log(n + " is a prime divisor")
+    }
+}
